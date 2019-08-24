2ce7ca2895a15554676ecb2089e9ea4147ff8e75
junfei <junfei@qq.com>
2019-08-24 14:45:26 +0800

    add curl log

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -292,3 +292,115 @@ function alarm($content){
     ];
     addQueue("Index","monitor","alarm",$qHistoryMsgParameter);
 }
+
+/**
+ * 记录curl请求日志
+ $url  = "https://www.baidu.com/";
+                $params = [];
+                $params['amount'] = 10;
+                $params = json_encode($params);
+                $headers[] = 'APP-Key: APP Key';
+                $headers[] = 'Content-Type: application/json; charset=UTF-8';
+                $ret       = curl_post($url, $params, $headers);
+                $ret = json_decode($ret,true);
+ */
+function curl_post($url, $data,$headers=[],$timeout=100){
+
+    $ch     = curl_init();
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //不输出内容到页面
+
+    //设置请求头响应头
+    curl_setopt($ch, CURLOPT_HEADER, true); //return header of response
+    curl_setopt($ch, CURLINFO_HEADER_OUT, TRUE); //get request header
+
+
+    //设置超时时间
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout - 2);
+    //curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727;cli-test)');
+
+    //curl_setopt($ch, CURLOPT_VERBOSE, 1);
+
+
+    //https证书
+    $CA = false;
+    $caCert = getcwd() . '/cacert.pem'; // CA根证书
+    $SSL    = substr($url, 0, 8) == "https://" ? true : false;
+    if ($SSL) {
+        if($CA){
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true); // 只信任CA颁布的证书
+            curl_setopt($ch, CURLOPT_CAINFO, $caCert); // CA根证书（用来验证的网站证书是否是CA颁布）
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); // 检查证书中是否设置域名，并且是否与提供的主机名匹配
+        }else{
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 信任任何证书
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // 检查证书中是否设置域名
+        }
+    }
+
+    //代理
+    //if(C('test_proxy')) {
+    //curl_setopt($ch, CURLOPT_PROXY, '192.168.16.96:8888');
+    //}
+
+    $ret = curl_exec($ch);
+    //var_dump($ret);exit('xx');
+    if (empty($ret)) {
+        var_dump(curl_error($ch)); // 查看报错信息
+        writeCurlResponseLog(curl_error($ch));
+        return false;
+    }
+
+    //请求信息,如果网络异常也没有请求信息
+    $requestHeader = curl_getinfo($ch,CURLINFO_HEADER_OUT);
+    if(is_string($data)){
+        $requestBody = $data;
+    }else{
+        $requestBody = var_export($data,1);
+    }
+    try{
+        writeCurlRequestLog($url,'post',$requestHeader.$requestBody);
+    }catch (Exception $e){
+
+    }
+
+    //响应头
+    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $responseHeaders = substr($ret, 0, $header_size);
+    $responseBody = substr($ret, $header_size);
+    //var_dump($responseHeaders);
+    try{
+        writeCurlResponseLog($ret);
+    }catch (Exception $e){
+
+    }
+    curl_close($ch);
+    return $responseBody;
+
+}
+
+function writeCurlRequestLog($url,$method,$requestData){
+    $data = [];
+    $data['url'] = $url;
+    $data['method'] = $method;
+    $data['detail'] = $requestData;
+    $sql = "INSERT INTO ".'log_curl'." (`url`,`method`,`detail`) VALUES('{$data['url']}','{$data['method']}','{$data['detail']}')";
+    pdo_query($sql);
+    //pdo_insert("log_curl", $requestData);
+    $logId = pdo_insertid();
+    global $_W;
+    $_W['curlLogId'] = $logId;
+    header("logId: $logId");
+    return $logId;
+
+}
+
+function writeCurlResponseLog($responseData){
+    global $_W;
+    $sql = "update  ".'log_curl'." set response='{$responseData}'";
+    //echo $sql;
+    pdo_query($sql);
+}
